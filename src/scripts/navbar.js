75b33437201bdf7cc1bb90d2c896b6c220dc785d
j-rsh <aroosha.jamlooii@gmail.com>
2024-06-01 20:51:39 +0330

    fix: add border to navbar links

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const hamburger = document.querySelector('.hambergur-menu');
 	const links = document.querySelector('.nav-links');
+	const x = document.querySelectorAll('.main-banner ul li a');
+
   
 	hamburger.addEventListener('click', () => {
 	  links.style.display = links.style.display === 'none'? 'flex' : 'none';
@@ -9,12 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('close-menu').addEventListener('click', function() {
 		links.style.display ='none';
 	  });
+
+	  x.forEach(link => {
+        link.addEventListener('click', function(e) {            
+            x.forEach(link => link.classList.remove('active'));
+            
+            this.classList.add('active');
+        });
+    });
   });
 
   document.getElementById('bookBtn').addEventListener('click', function() {
-    document.getElementById('form').scrollIntoView({behavior: "smooth"});
+    document.getElementById('book').scrollIntoView({behavior: "smooth"});
 });
 
 document.getElementById('menuBtn').addEventListener('click', function() {
     document.getElementById('menu').scrollIntoView({behavior: "smooth"});
 });
+
